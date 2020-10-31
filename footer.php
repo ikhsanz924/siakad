26d2df98b83559518ee780abcfd4761234895d02
ikhsanz924 <mikhsanz924@gmail.com>
2020-10-31 18:58:00 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div class="pull-right hidden-xs">
     <b></b>
 </div>
-<strong>Copyright &copy; <?php echo date('Y'); ?> <a target='_BLANK' href="">- ISSCO (Integrated System Smart School)</a>.</strong> All rights reserved.
+<strong>Copyright &copy; <?php echo date('Y'); ?> <a target='_BLANK' href="index.php">- ISSCO (Integrated System Smart School)</a>.</strong> All rights reserved.
